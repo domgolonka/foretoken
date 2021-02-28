ec95ae8a1bf8e55388fd87a3462f03dd0f392715
Dominik Golonka <domgolonka@gmail.com>
2021-02-28 08:42:08 -0800

    etcd3

--- lib/grpc/grpc_server.go
+++ lib/grpc/grpc_server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/domgolonka/foretoken/app"
 	"github.com/domgolonka/foretoken/lib/grpc/impl"
 	"github.com/domgolonka/foretoken/lib/grpc/proto"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"google.golang.org/grpc"
 )
 
@@ -18,6 +19,8 @@ func ServeRPC(app *app.App, ch chan bool, address string) {
 	}
 	proto.RegisterEmailServiceServer(s, impl.EmailService)
 	proto.RegisterIPServiceServer(s, impl.IPService)
+
+	grpc_prometheus.Register(s)
 
 	if err = s.Serve(l); err != nil {
 		ch <- false
